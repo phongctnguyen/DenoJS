e22172232307ad866b655a3ea02110c3102bb145
phongctnguyen <phongctnguyen@gmail.com>
2020-06-21 21:57:27 +0700

    finish typescript section

--- 4. Bonus: Typescript/type.js
+++ 4. Bonus: Typescript/type.js
@@ -3,20 +3,20 @@ var isCool = true;
 // number
 var age = 56;
 // string
-var myName = 'Phong';
+var myName = "Phong";
 var favouriteQuote = "I'm " + age + " years old";
 // arrays
-var pets = ['cat', 'dog', 'elephant'];
-var pets2 = ['lion', 'tiger'];
+var pets = ["cat", "dog", "elephant"];
+var pets2 = ["lion", "tiger"];
 // object
 var wizard = {
-    a: 'John'
+    a: "John"
 };
 // null & undefined
 var meh = undefined;
 var boo = null;
 // tuple
-var basket = ['basketball', 5];
+var basket = ["basketball", 5];
 // enum
 var Size;
 (function (Size) {
@@ -24,22 +24,49 @@ var Size;
     Size[Size["Medium"] = 2] = "Medium";
     Size[Size["Large"] = 3] = "Large";
 })(Size || (Size = {}));
-;
 var sizeName = Size.Small;
 // any -- Be Careful
 var whatever = "hello";
 whatever = 6;
 // void
 var sing = function () {
-    console.log('Hello');
+    console.log("Hello");
 };
 // never
 var error = function () {
     throw Error("Oops");
 };
 var fightRobotArmy = function (robots) {
-    console.log('Fight');
+    console.log("Fight");
 };
+fightRobotArmy({ count: 1, type: "dragon" });
 var fightRobotArmy2 = function (robots) {
-    console.log('Fight');
+    console.log("Fight");
 };
+var dog = {};
+dog.count;
+// function
+var fightRobotArmy3 = function (robots) {
+    console.log("Fight");
+};
+var fightRobotArmy4 = function (robots) {
+    console.log("Fight");
+    return 5;
+};
+// classes
+var Animal = /** @class */ (function () {
+    // public sing: string = 'lalala';
+    function Animal(sound) {
+        this.sing = 'lalala';
+        this.sing = sound;
+    }
+    Animal.prototype.greet = function () {
+        return "Hello " + this.sing;
+    };
+    return Animal;
+}());
+var lion = new Animal('RAAAAWR');
+lion.greet();
+// union
+var confused = "hello";
+confused = 4;
--- 4. Bonus: Typescript/type.ts
+++ 4. Bonus: Typescript/type.ts
@@ -5,27 +5,31 @@ let isCool: boolean = true;
 let age: number = 56;
 
 // string
-let myName: string = 'Phong';
+let myName: string = "Phong";
 let favouriteQuote: string = `I'm ${age} years old`;
 
 // arrays
-let pets: string[] = ['cat', 'dog', 'elephant'];
-let pets2: Array<string> = ['lion', 'tiger'];
+let pets: string[] = ["cat", "dog", "elephant"];
+let pets2: Array<string> = ["lion", "tiger"];
 
 // object
 let wizard: object = {
-  a: 'John'
-}
+  a: "John",
+};
 
 // null & undefined
 let meh: undefined = undefined;
 let boo: null = null;
 
 // tuple
-let basket: [string, number] = ['basketball', 5];
+let basket: [string, number] = ["basketball", 5];
 
 // enum
-enum Size { Small = 1, Medium = 2, Large = 3 };
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
 let sizeName: number = Size.Small;
 
 // any -- Be Careful
@@ -34,25 +38,68 @@ whatever = 6;
 
 // void
 let sing = (): void => {
-  console.log('Hello');
-}
+  console.log("Hello");
+};
 
 // never
 let error = (): never => {
   throw Error("Oops");
-}
+};
 
 // interface
 interface RobotArmy {
-  count: number,
-  type: string,
-  magic: string
+  count: number;
+  type: string;
+  magic?: string;
 }
 
 let fightRobotArmy = (robots: RobotArmy) => {
-  console.log('Fight');
+  console.log("Fight");
+};
+fightRobotArmy({ count: 1, type: "dragon" });
+
+let fightRobotArmy2 = (
+  robots: { count: number; type: string; magic: string },
+) => {
+  console.log("Fight");
+};
+
+// type assertion
+interface CatArmy {
+  count: number;
+  type: string;
+  magic: string;
 }
 
-let fightRobotArmy2 = (robots: {count: number, type: string, magic: string}) => {
-	console.log('Fight')
+let dog = {} as CatArmy;
+dog.count;
+
+// function
+let fightRobotArmy3 = (robots: RobotArmy): void => {
+  console.log("Fight");
+};
+
+let fightRobotArmy4 = (robots: { count: number; type: string; magic: string }): number => {
+  console.log("Fight");
+  return 5;
+};
+
+// classes
+class Animal {
+  private sing: string = 'lalala';
+  // public sing: string = 'lalala';
+  constructor(sound: string) {
+    this.sing = sound;
+  }
+
+  greet() {
+    return `Hello ${this.sing}`;
+  }
 }
+
+let lion = new Animal('RAAAAWR');
+lion.greet();
+
+// union
+let confused: string | number | boolean = "hello";
+confused = 4;
